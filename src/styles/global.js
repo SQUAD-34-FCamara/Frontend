b0246569fc110b1b6b5eeeff1c13d648670cd489
GisellePiasetzki <piasetzki.giselle@gmail.com>
2022-04-07 21:17:47 -0300

    Adicionando fonte global

--- src/styles/global.js
+++ src/styles/global.js
@@ -22,6 +22,16 @@ export const GlobalStyle = createGlobalStyle `
         }
     }
 
+    body, input, textarea, button {
+        font-family: 'Montserrat', sans-serif;
+        font-weight: 400;
+    }
+
+    h1, h2, h3, h4, h5, h6, strong{
+        font-weight: 600;
+    }
+
+
     button {
         cursor: pointer;
     }
